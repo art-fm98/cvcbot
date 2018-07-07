dfb6967dba03dcaf9ff3cba4d6c47be25c4da17f
art-fm98 <40850614+art-fm98@users.noreply.github.com>
2018-07-07 14:23:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
  
-$strAccessToken = "9eKp8vy8uS5ZFWy+PAnIqYqZJjifr/k8h1iiRQiNn9zRHIOCwWdbT0CYDjZWJSk72uWa78W12A0BafcwDagfATUhkfEo0vFnvoPkNBmJKlLNIVPr1Hh5/h1eR3v4hapy14agCWtNQybHiL3pApWFrwdB04t89/1O/w1cDnyilFU=
+$strAccessToken = "Tn06kA54O9TDusL2993NA6H6u1/Sjv5KblMz53V7f35O0PrrV2fUECzrSktCUxm32uWa78W12A0BafcwDagfATUhkfEo0vFnvoPkNBmJKlJZcrtm3nhZVxhonPKIJnTeucpdICydKorZpEOWE8Lb0wdB04t89/1O/w1cDnyilFU=
 ";
  
 $content = file_get_contents('php://input');
